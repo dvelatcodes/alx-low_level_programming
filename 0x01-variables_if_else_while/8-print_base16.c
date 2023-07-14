4eb73862d4f064a4f809b13d5e7ec077997882d3
love olubummo <olubummolove3@gmail.com>
2023-07-14 14:14:30 -0700

    initial 8-print_base16.c file

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int nums = 48;
+
+	char letss = 'a';
+
+	while (letss <= 'f')
+	{
+		while (nums < 58)
+		{
+			putchar(nums);
+			nums++;
+		}
+		if (nums >= 58)
+		{
+			putchar(letss);
+			letss++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}	
