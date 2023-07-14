44b687db197afabf9deab26392f53d31da6f0e11
love olubummo <olubummolove3@gmail.com>
2023-07-14 13:09:14 -0700

    5-print_numbers.c file that prints all single digit numbers of base 10 starting from 0, followed by a new line.

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print all numbers from 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+	return (0);
+}
