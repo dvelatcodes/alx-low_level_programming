e3f1290f47d5eb8f96ade117b3f7216608cb97c5
love olubummo <olubummolove3@gmail.com>
2023-07-14 11:50:04 -0700

    initial 3-print_alphabets.c file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - print upper and lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lower, upper;
+
+	lower = 'a';
+	upper = 'A';
+
+	while (upper <= 'Z')
+	{
+		while (lower <= 'z')
+		{
+			putchar(lower);
+			lower++;
+		}
+		if (lower == 'z')
+		{
+			putchar(upper);
+			upper++;
+		}
+		putchar('\n');
+	}
+	return (0);
+}
