346448d83764656e66fa4bd05767779f0c2b4f2a
love olubummo <olubummolove3@gmail.com>
2023-07-19 08:44:26 -0700

    made changes to 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,17 +6,37 @@
  */
 void print_to_98(int n)
 {
-	if (n >= 98)
-	{
-		while (n > 98)
-			printf("%d, ", n--);
-		printf("%d\n", n);
-	}
+	int check1 = 0, check2 = 0, check3 = 0;
 
-	else
+
+	if (n > 98 && check2 == 0 && check3 == 0)
 	{
-		while (n < 98)
-			printf("%d, ", n++);
-		printf("%d\n", n);
+		while (n >= 98)
+		{
+			_putchar(n + 48);
+			_putchar(44);
+			_putchar(32);
+			n--;
+			if (n == 97)
+			check1 = 1;
+		}
 	}
+	if (n < 98 && check1 == 0 && check3 == 0)
+	{
+		while (n <= 98)
+		{
+			_putchar(n + 48);
+			_putchar(44);
+			_putchar(32);
+			n++;
+			if (n == 99)
+			check2 = 1;
+		}
+	}
+	if (n == 98 && check1 == 0 && check2 == 0)
+	{
+		_putchar(n + 48);
+		check3 = 1;
+	}
+	_putchar('\n');
 }
